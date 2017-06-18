6b9d3cab899584277f336bd50109028f68a94d95
mariusz88maniek88 <mariuszborkowski1988@tutanota.com>
2017-06-19 01:39:43 +0200

    0.6.1

--- login.php
+++ login.php
@@ -1,6 +1,13 @@
 <?php 
 session_start();
 
+if(!isset($_SESSION['login'])) {
+    
+    header("Location:index.php");
+    exit();
+    
+}
+
 require_once 'sys\main.php';
 require_once 'db_connect.php';
 
